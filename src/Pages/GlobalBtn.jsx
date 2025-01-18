92b8d3f118e38c40b01463688ceb0ead07270775
daniyalk0 <getdaniyalkhan@gmail.com>
2025-01-18 19:34:45 +0530

    fix

--- src/Pages/GlobalBtn.jsx
+++ src/Pages/GlobalBtn.jsx
@@ -6,7 +6,7 @@ function GlobalBtn() {
 
   return (
     <button  className="btn py-[0.8vw] text-[1vw] px-8 font-[anzo6] bg-black text-white rounded-tl-3xl rounded-br-3xl rounded-bl-sm rounded-tr-sm  border-[0.33vw] border-white hover:bg-[#707070] transition-all duration-300 cursor-pointer shadow-md shadow-zinc-700">
-      GlobalBtn
+      Hire Me
     </button>
   );
 }
